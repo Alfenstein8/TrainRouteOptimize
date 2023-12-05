f7d7e3cc976f1bb7e6f2307df0c175a7b6205a52
Alfred Makne Poulsen <=>
2023-12-05 11:36:23 +0100

    Removed air_time main() and updated makefile

--- air_travel_time/air_travel_time.c
+++ air_travel_time/air_travel_time.c
@@ -2,26 +2,7 @@
 
 #include "../sim_file/sim_file.h"
 
-
-int main(void){
-
-/* Test data
-SimFile test = {
-    .flight_time_min = 70,
-    .airport_prep_time_min = 60,
-};
-int travel_time_origin_destination = 40;
-
-// Call function to get total air travel time
-int total_travel_time_air = GetTotalAirTravelTime(test, travel_time_origin_destination);
-printf("Test: %d\n", total_travel_time_air);
-
-
-return 0;
-
-} */
 // Function to get the total air travel time
-}
 int get_total_air_travel_time(SimFile sim_data, int travel_time_origin_destination){ 
 
 // Calculates the
