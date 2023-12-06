8802ff8c277c19c670553aa0c566ae62ce8f9255
KLJ <=>
2023-12-06 13:10:10 +0100

    Added code note

--- air_travel_time/air_travel_time.c
+++ air_travel_time/air_travel_time.c
@@ -2,10 +2,12 @@
 
 #include "../sim_file/sim_file.h"
 
-// Function to get the total air travel time
+/* Function to get the total air travel time */
 int get_total_air_travel_time(SimFile sim_data, int travel_time_origin_destination){ 
 
-// Calculates the
+/* Calculates the total air time taking into account air flight time, air preparation time and..
+*  travel time from origin to start airport & end airport to destination 
+*/
 int air_route_travel_time;
 
 air_route_travel_time += sim_data.flight_time_min;
