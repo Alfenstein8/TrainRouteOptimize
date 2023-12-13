40b70f7f0d183cfc46fd4671a64046286a42a693
Alfred Makne Poulsen <alfred@omj.dk>
2023-12-13 11:20:09 +0100

    added calculate_alle_interaction_levels function to the rest of the program

--- calc_all_interaction_levels/calc_interaction.c
+++ calc_all_interaction_levels/calc_interaction.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* calculate_all_interaction_levels(int table_size, int **OD_table) {
-    /*Allokerer hukommelse til interationsniveauet*/
-    int* interaction_levels = (int*)malloc(table_size * sizeof(int));
+int *calculate_all_interaction_levels(int table_size, int *OD_table) {
+  /*Allokerer hukommelse til interationsniveauet*/
+  int *interaction_levels = (int *)malloc(table_size * sizeof(int));
 
-    /*Initialiserer løkken med nul-værdier for hver station, for reuseability*/
-    for (int i = 0; i < table_size; ++i) {
-        interaction_levels[i] = 0;
+  if (interaction_levels == NULL) {
+    printf("Could not allocate memory!");
+    exit(EXIT_FAILURE);
+  }
+
+  /*Initialiserer løkken med nul-værdier for hver station, for reuseability*/
+  for (int i = 0; i < table_size; ++i) {
+    interaction_levels[i] = 0;
+  }
+
+  /*Beregner interaktionsniveauerne*/
+  for (int i = 0; i < table_size; ++i) {
+    for (int j = 0; j < table_size; ++j) {
+      if (i == j) {
+        continue;
+      }
+      interaction_levels[i] += *((OD_table + i * table_size) + j);
+      interaction_levels[j] += *((OD_table + i * table_size) + j);
     }
+  }
 
-    /*Beregner interaktionsniveauerne*/
-    for (int i = 0; i < table_size; ++i) {
-        for (int j = 0; j < table_size; ++j) {
-            if (i == j){
-                continue;
-            }
-            interaction_levels[i] += OD_table[i][j];
-            interaction_levels[j] += OD_table[i][j];
-        }
-    }
-
-    return interaction_levels;
+  return interaction_levels;
 }
