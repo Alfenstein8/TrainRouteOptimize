09c27137a6190707a9a68c4d7e6da734320e6d83
Arian Wardak <arianwardak@gmail.com>
2023-12-14 14:45:55 +0100

    Changed the linesegment to an int array containing the time travled to each station and removed turnover time from function

--- create_line/create_new_line.c
+++ create_line/create_new_line.c
@@ -3,18 +3,13 @@
 #include <stdlib.h>
 #include "../rails/rails.h"
 
-struct LineSegment {
-    double speed; //  km/min
-    double total_time; // min
-};
-typedef struct LineSegment LineSegment;
 
-LineSegment create_line_segment(int top_speed, double acceleration, double distance, int turnover_time);
+double create_line_segment(int top_speed, double acceleration, double distance);
 
 
-LineSegment* create_new_line(double HST_acceleration, Rail new_rails[], int num_stations,int turnover_time) {
+double* create_new_line(double HST_acceleration, Rail new_rails[], int num_stations) {
 
-     LineSegment* new_line = (LineSegment *) malloc (num_stations * sizeof(LineSegment));
+     double* new_line = (double *) malloc (num_stations * sizeof(double));
 
      HST_acceleration *= 3.6; // convert from meter per second squared to kilometers per minute squated
 
@@ -22,9 +17,7 @@ LineSegment* create_new_line(double HST_acceleration, Rail new_rails[], int num_
         int top_speed = (new_rails[i].top_speed)/60; // convert from km/t to km/m
         int length = new_rails[i].length;
 
-        
-
-        new_line[i] = create_line_segment(top_speed, HST_acceleration, length, turnover_time);
+        new_line[i] = create_line_segment(top_speed, HST_acceleration, length);
     }
 
     return new_line;
@@ -32,17 +25,15 @@ LineSegment* create_new_line(double HST_acceleration, Rail new_rails[], int num_
 
 
 
-LineSegment create_line_segment(int top_speed, double acceleration, double distance, int turnover_time) {
-   LineSegment segment;
+double create_line_segment(int top_speed, double acceleration, double distance) {
+   double segment;
 
     double time_not_at_top_speed = top_speed / acceleration;
     double distance_not_at_top_speed = 0.5 * acceleration * pow(time_not_at_top_speed, 2);
     double distance_at_top_speed = distance - 2*distance_not_at_top_speed;
     double time_at_top_speed = distance_at_top_speed / top_speed;
     
-
-    segment.speed = top_speed/60;  // convert from km/t to km/m
-    segment.total_time = 2*time_not_at_top_speed + time_at_top_speed + turnover_time; 
+    segment = 2*time_not_at_top_speed + time_at_top_speed; 
 
     return segment;
 }
