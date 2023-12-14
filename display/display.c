d57b553047742eb703069d5294125691971af6e7
Masuma Hussaini <=>
2023-12-14 09:29:33 +0100

    Removed main

--- display/display.c
+++ display/display.c
@@ -6,9 +6,3 @@ void print_travel_times(int hst_travel_time_min, int city_train_total_time_min, 
     printf("Plane Travel Time:            %d hour(s) and %d minutes\n", plane_travel_time_min / 60, plane_travel_time_min % 60);
     printf("Offset Time:                  %d hour(s) and %d minutes\n", offset_time_min / 60, offset_time_min % 60);
 }
-
-int main() {
-    // Test
-    print_travel_times(127, 98, 60, 30);
-    return 0;
-}
