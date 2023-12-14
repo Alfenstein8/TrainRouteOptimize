0e1f7fbaafd144b439f5394d38254318dc6e8204
Arian Wardak <arianwardak@gmail.com>
2023-12-14 13:55:45 +0100

    Offset function changed

--- Offset/offset.c
+++ Offset/offset.c
@@ -3,38 +3,35 @@
    #include "../rails/rails.h"
 
 
-struct Line {
-    int num_segments;
-    struct Rail segments[9]; // max size will always be 9 in our example route
-};
-
- int calculate_departure_offset(struct Line *ICL_train_line, struct Line *HST_train_line) {
-    int ICL_start_time = 0;
-    int HST_start_time = 5;
+ int calculate_departure_offset(double new_line[], double icl_line[]) {
     double HST_arrival_times[9]; 
-    double HST_arrival_times[0] = HST_start_time; 
+      for (int k = 1; k < 60; k++){
+         int HST_start_time = k;
+         HST_arrival_times[0] = HST_start_time; 
+     
 
-    for (int i = 1; i < HST_train_line->num_segments; ++i) {
-        double HST_speed = HST_train_line->segments[i].top_speed;
-        int HST_distance = HST_train_line->segments[i].length;
+      for (int i = 1; i  <9 ; ++i) {
 
         // Calculate time taken to reach the current segment
+
         double time_taken = new_line[i-1];
 
          HST_arrival_times[i] = HST_arrival_times[i - 1] + time_taken; 
     }
 
          double ICL_arrival_times[9];  // the defined route will always have 9 stops in our example
+          int ICL_start_time = 0;
          ICL_arrival_times[0] = ICL_start_time;
-         for (int i = 1; i < ICL_train_line->num_segments; ++j) {
+
+         for (int i = 1; i < 9; ++i) {
          double time_taken = ICL_line[i-1];
 
             ICL_arrival_times[i] = ICL_arrival_times[i - 1] + time_taken;
    }
-     for (int i = 1; i < HST_train_line->num_segments; ++i) {
+     for (int i = 1; i < 9; ++i) {
         int HST_arrival_time = HST_arrival_times[i];
 
-        for (int j = 1; j < ICL_train_line->num_segments; ++j) {
+        for (int j = 1; j < 9; ++j) {
             if (ICL_arrival_times[j] == HST_arrival_time) {
                 return i;
             }
@@ -42,7 +39,7 @@ struct Line {
     }
    }
      // Calculate the index where arrival times match
-    int index = calculate_departure_offset(&ICL_train_line, &HST_train_line);
+    int index = calculate_departure_offset(new_line[], icl_line[]);
 
     if (index != -1) {
         printf("Matching segment index: %d\n", index);
@@ -50,3 +47,4 @@ struct Line {
         printf("No matching segment found.\n");
     }
 
+ }
