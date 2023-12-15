8c4141ac8229030458f296c08f9e232a4654865b
Niko <nikolajriel@hotmail.com>
2023-12-15 10:33:21 +0100

    Included changes made with Peter's function + a main with a print added in as a comment

--- calc_all_interaction_levels/calc_interaction.c
+++ calc_all_interaction_levels/calc_interaction.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*#include "load_od_table.h"*/
 
-int *calculate_all_interaction_levels(int table_size, int *OD_table) {
+int *calculate_all_interaction_levels(int table_size, int **OD_table);
+
+/*
+int main(void){
+    char *route[4] = {"Albertslund", "Alleroed", "Avedoere", "Bernstorffsvej"};
+    char filename[] = "OD_modified.csv";
+    int *interaction_levels_table;
+    int route_length = sizeof(route) / sizeof(route[0]);
+    int **loaded_od_table = load_od_table(route, filename);
+    interaction_levels_table = calculate_all_interaction_levels(route_length, loaded_od_table);
+
+    for(int i = 0; i < route_length; i++){
+    printf("%d\n", interaction_levels_table[i]);
+    }
+
+    int b;
+    for (b = 0; b < 289; b++) {
+        free(loaded_od_table[b]);
+    }
+    free(loaded_od_table);
+
+    free(interaction_levels_table);
+
+  return 0;
+}
+*/
+
+int *calculate_all_interaction_levels(int table_size, int **OD_table) {
+    
   /*Allokerer hukommelse til interationsniveauet*/
   int *interaction_levels = (int *)malloc(table_size * sizeof(int));
 
@@ -21,8 +50,8 @@ int *calculate_all_interaction_levels(int table_size, int *OD_table) {
       if (i == j) {
         continue;
       }
-      interaction_levels[i] += *((OD_table + i * table_size) + j);
-      interaction_levels[j] += *((OD_table + i * table_size) + j);
+      interaction_levels[i] += OD_table[i][j];
+      interaction_levels[j] += OD_table[i][j];
     }
   }
 
