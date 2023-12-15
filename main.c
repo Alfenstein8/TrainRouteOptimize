ed9f47d8d7a4c4b182d6913ffdb9efd8b0977f29
Alfred Makne Poulsen <=>
2023-12-15 10:59:54 +0100

    Added feedback when not entering file

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "application.h"
 #include "userfile_generator/userfile_generator.h"
+#include <stdio.h>
 #include <string.h>
 
 int main(const int argc, char **argv) {
 
+  if (argc <= 1) {
+    printf("No file entered");
+    return 0;
+  }
   int found_gen = 0;
   for (int i = 1; i < argc; ++i) {
     if (!strcmp(argv[i], "--gen")) {
