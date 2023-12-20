f9b29bede58baef41882497901221b72bf5b02cd
Alfred Makne Poulsen <alfred@omj.dk>
2023-12-20 19:38:26 +0100

    Refactored application.c by renaming variables and extracted functions

--- application.c
+++ application.c
@@ -18,51 +18,48 @@
 #define MAX_NUMBER_OF_STATIONS 10
 
 int get_total_time(double *line, int size, int turnover_time);
+void free_2darray(void **array, int size);
 
 void run(const char *file_path) {
-  SimFile sim_file_data;
+  SimFile sim_file;
 
-  load_local_file(&sim_file_data, file_path);
+  load_local_file(&sim_file, file_path);
 
   char *stations[MAX_NUMBER_OF_STATIONS];
-  const int num_of_stations = api_get_route("København", "Aalborg", stations);
+  const int icl_line_length = api_get_route("København", "Aalborg", stations);
 
-  Rail *rails = load_rails("rails.csv", stations, num_of_stations);
+  Rail *all_rails = load_rails("rails.csv", stations, icl_line_length);
 
-  int **loaded_od_table = load_od_table(stations, num_of_stations, "OD_modified.csv");
+  int **od_table = load_od_table(stations, icl_line_length, "OD_modified.csv");
+  int *interaction_levels = calculate_all_interaction_levels(icl_line_length, od_table);
+  free_2darray((void *)od_table, icl_line_length);
 
-  int *interaction_levels = calculate_all_interaction_levels(num_of_stations, loaded_od_table);
+  int hst_station_numbers[icl_line_length];
+  int hst_line_length =
+      remove_low_interaction_stations(interaction_levels, icl_line_length,
+                                      sim_file.station_removal_percentage, hst_station_numbers);
 
-  for (int i = 0; i < num_of_stations; i++) {
-    free(loaded_od_table[i]);
-  }
-  free(loaded_od_table);
+  Rail *hst_rails = make_new_rails(all_rails, hst_station_numbers, hst_line_length);
 
-  int hst_route[num_of_stations];
-  int hst_route_size = remove_low_interaction_stations(
-      interaction_levels, num_of_stations, sim_file_data.station_removal_percentage, hst_route);
+  double *hst_times = create_new_line(sim_file.acceleration, hst_rails, hst_line_length,
+                                      sim_file.hst_top_speed_kmt);
+  double *icl_times =
+      create_new_line(sim_file.acceleration, all_rails, icl_line_length, sim_file.icl_top_speed);
 
-  Rail *new_rails_hst = make_new_rails(rails, hst_route, hst_route_size);
+  free(all_rails);
 
-  double *hst_line = create_new_line(sim_file_data.acceleration, new_rails_hst, hst_route_size, sim_file_data.hst_top_speed_kmt);
-
-  double *icl_line = create_new_line(sim_file_data.acceleration, rails, num_of_stations, sim_file_data.icl_top_speed);
-
-  free(rails);
-
-  int travel_time_origin_destination = get_od_time();
-
-  int air_travel_time = get_total_air_travel_time(sim_file_data, travel_time_origin_destination);
+  int misc_travel_time = get_od_time(); // miscellaneous travel time
+  int air_travel_time = get_total_air_travel_time(sim_file, misc_travel_time);
 
   int offset_time =
-      calculate_departure_offset(new_rails_hst, rails, hst_line, icl_line, hst_route_size,
-                                 num_of_stations, sim_file_data.turnover_time);
+      calculate_departure_offset(hst_rails, all_rails, hst_times, icl_times, hst_line_length,
+                                 icl_line_length, sim_file.turnover_time);
 
-  int new_line_time = get_total_time(hst_line,hst_route_size, sim_file_data.turnover_time);
-  int original_line_time = get_total_time(icl_line,num_of_stations, sim_file_data.turnover_time);
+  int new_line_time = get_total_time(hst_times, hst_line_length, sim_file.turnover_time);
+  int original_line_time = get_total_time(icl_times, icl_line_length, sim_file.turnover_time);
   print_travel_times(new_line_time, original_line_time, air_travel_time, offset_time);
 
-  free(new_rails_hst);
+  free(hst_rails);
 }
 
 int get_total_time(double *line, int size, int turnover_time) {
@@ -71,4 +68,11 @@ int get_total_time(double *line, int size, int turnover_time) {
     time += line[i] + turnover_time;
   }
   return time;
+}
+
+void free_2darray(void **array, int size) {
+  for (int i = 0; i < size; i++) {
+    free(array[i]);
+  }
+  free(array);
 }
